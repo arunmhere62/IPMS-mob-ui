0f1d09c9a84ed614373b306c5ccc29998191763c
arunmhere62 <arunmhere62@gmail.com>
2026-01-07 09:08:52 +0530

    ui changes

--- app.config.js
+++ app.config.js
@@ -23,6 +23,7 @@ module.exports = ({ config }) => {
     ...baseExpoConfig,
     android: {
       ...(baseExpoConfig.android ?? {}),
+      usesCleartextTraffic: true,
       intentFilters: [
         ...((baseExpoConfig.android?.intentFilters ?? [])),
         paymentResultIntentFilter,
@@ -47,8 +48,8 @@ module.exports = ({ config }) => {
       },
       appEnv: (process.env.APP_ENV || process.env.MODE || 'dev').toLowerCase(),
       // Single source of truth for API URL - change in .env file
-      // apiBaseUrl: process.env.API_BASE_URL || "https://pg-api-mobile.vercel.app/api/v1",
-      apiBaseUrl: process.env.API_BASE_URL || "http://172.20.10.2:5000/api/v1",
+      apiBaseUrl: process.env.API_BASE_URL || "http://72.62.194.189:3000/api/v1",
+      // apiBaseUrl: process.env.API_BASE_URL || "http://172.20.10.2:5000/api/v1",
       // Subscription Configuration
       subscriptionMode: process.env.SUBSCRIPTION_MODE === 'true',
       showDevBanner: process.env.SHOW_DEV_BANNER === 'true'
